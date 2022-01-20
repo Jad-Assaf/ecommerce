188627c2c58afb550ae12b91f7fee125de8a7657
Wh0o0o <73811148+Wh0o0o@users.noreply.github.com>
2022-01-20 17:25:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.use(adminProductsRouter);
 app.use(productsRouter);
 app.use(cartsRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Listening');
 });
